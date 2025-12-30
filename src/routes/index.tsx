b557d8d00e5bef81ed38e6ea07391cfea1132d68
steve-rops <somebiss@gmail.com>
2025-12-30 02:52:30 +0200

    #2 commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,52 @@
+import { createFileRoute } from '@tanstack/react-router'
+import Header from '@/components/Header'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+      <div className="h-96">test</div>
+    </div>
+  )
+}
